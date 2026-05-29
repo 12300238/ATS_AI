a6e8d53be178b14dc768795b78dcfc5b97368793
12300238 <prades.mathew@gmail.com>
2026-05-29 14:49:49 +0200

    feat: initialisation de la base user mongoDB

---
+++ backend/mongodb/init.py
@@ -0,0 +1,13 @@
+import pymongo
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+
+# creation de la base de données
+user = myclient["user"]
+
+# creation des collections
+comptes = user["comptes"] #les compte
+
+offres = user["offres"] #les offres
+
+postulation = user["postulation"] #les postualtion au offres
